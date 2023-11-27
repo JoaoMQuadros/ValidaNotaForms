ca9adec845a78323b7347fa9535c816caeea5340
João Matheus De Quadros <joao.quadros@globalcobranca.com.br>
2023-11-27 16:02:45 -0300

    Adicionando formulário ao git

---
+++ NovoProjetoValidaNotasForms/Aluno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoProjetoValidaNotasForms
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public List<Notas> notas = new List<Notas>();
+        public List<Media> media = new List<Media>();
+    }
+    public class Notas
+    {
+        public int NotaMat { get; set; }
+        public int NotaHis { get; set; }
+        public int NotaPor { get; set; }
+        public int NotaArt { get; set; }
+        public int NotaFis { get; set; }
+        public int NotaIng { get; set; }
+    }
+    public class Media
+    {
+        public int MediaMat { get; set; }
+        public int MediaPor { get; set; }
+        public int MediaHis { get; set; }
+        public int MediaArt { get; set; }
+        public int MediaFis { get; set; }
+        public int MediaIng { get; set; }
+    }
+}
---
+++ NovoProjetoValidaNotasForms/Validador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public class Validador
+{
+    public int validaNotas(string entrada)
+    {
+        int comparador = 0;
+        if (int.TryParse(entrada, out comparador) && comparador >= 0 && comparador <= 10)
+        {
+            //Entrada validada
+        }
+        else
+        {
+            MessageBox.Show("Por favor digite apenas números entre 0  a 10");
+        }
+            return comparador;
+    }
+}
